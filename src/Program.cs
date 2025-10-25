cb1dca67116d5ebe517f53f9a8846452d2e2036d
Marlon Lima <goncalvesmarlon072@gmail.com>
2025-10-25 11:40:19 -0300

    Finalizando o desafio

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,15 @@
+using BugStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlite(connectionString);
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
 app.Run();
